35377fb3df59e215b3ce4fa130ed0124ac432957
dvssai <122370890+dvssai@users.noreply.github.com>
2026-06-07 19:52:22 +0530

    fct_orders invalid product ids removed

--- Ecommerce/models/mart/fct_orders.sql
+++ Ecommerce/models/mart/fct_orders.sql
@@ -1,11 +1,26 @@
 with fct_orders as (
-        Select * from {{ref('Int_orders')}}
+        Select p.product_id as product_id,
+        order_id,
+        Customer_id,
+        order_status,
+        purchased_date,
+        delivery_date,
+        estimated_delivery_date,
+        price,
+        order_item_id,
+        seller_id,
+        freight_value,shipping_limit_date
+         from {{ref('Int_orders')}} O RIGHT JOIN {{ref('dim_products')}} P
+        on O.product_id = P.product_id
 )
 Select order_id,Customer_id as Customerid,
 order_status,
-purchased_date,
-delivery_date,
-estimated_delivery_date,
+purchased_date as purchase_tp,
+delivery_date as delivery_tp,
+estimated_delivery_date as estimated_delivery_tp,
+CAST(left(purchased_date,10) as DATE) as purchase_dt,
+CAST(left(delivery_date,10) as DATE) as delivery_dt,
+CAST(left(estimated_delivery_date,10) as DATE)  as estimated_delivery_dt,
 product_id,price,
 order_item_id,
 seller_id,freight_value,shipping_limit_date,
